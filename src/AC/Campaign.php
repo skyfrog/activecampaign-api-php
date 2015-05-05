f32e9f012f0853af85aef69c77a9bb5e6f9c79b6
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2015-05-05 09:22:35 +0200

    Fix API endpoints + specify access modifiers

--- src/AC/Campaign.php
+++ src/AC/Campaign.php
@@ -1,5 +1,6 @@
 <?php
 namespace AC;
+
 use AC\Models\CampaignPaginator;
 use AC\Models\Contact;
 use AC\Models\Campaign as CampaignM;
@@ -108,7 +109,7 @@ class Campaign extends ActiveCampaign
         $action = $this->getAction(
             __METHOD__,
             array(
-                'method'    => 'campaign_report_unopen_list',
+                'method'    => 'campaign_report_totals',
                 'data'      => array(
                     'campaignid'    => $campaign->getId(),
                 )
@@ -143,7 +144,7 @@ class Campaign extends ActiveCampaign
         $action = $this->getAction(
             __METHOD__,
             array(
-                'method'    => 'campaign_report_unopen_list',
+                'method'    => 'campaign_report_open_list',
                 'data'      => $data
             )
         );
@@ -186,133 +187,192 @@ class Campaign extends ActiveCampaign
         return $contacts;
     }
 
-    function create($params, $post_data)
+    /**
+     * @param CampaignM $campaign
+     * @return array
+     * @throws \RuntimeException
+     */
+    public function getUnsubscribeList(CampaignM $campaign)
+    {
+        $data = array(
+            'campaignid'    => $campaign->getId(),
+        );
+        if ($campaign->getMessageid()) {
+            $data['messageid'] = $campaign->getMessageid();
+        }
+        $action = $this->getAction(
+            __METHOD__,
+            array(
+                'method'    => 'campaign_report_unsubscription_list',
+                'data'      => $data
+            )
+        );
+        $opens = $this->doAction(
+            $action
+        );
+        if (!isset($opens->result_code) || $opens->result_code == '0')
+            throw new \RuntimeException(
+                sprintf(
+                    'Failed to get unopen list: (HTTP: %d) %s',
+                    isset($opens->http_code) ? $opens->http_code : 0,
+                    isset($opens->result_message) ? $opens->result_message : 'Unknown'
+                )
+            );
+        $contacts = array();
+        if ($campaign->getUniqueopens())
+        {//if we know how many opens there are ->
+            $j = $campaign->getUniqueopens();
+            for ($i=0;$i<$j;++$i)
+            {
+                if (isset($opens->{$i}))
+                {
+                    $contacts[] = new Contact(
+                        $opens->{$i}
+                    );
+                }
+            }
+        }
+        else
+        {//just keep incrementing $i until $opens->{$i} is not set
+            $i = 0;
+            while (isset($opens->{$i}))
+            {
+                $contacts[] = new Contact(
+                    $opens->{$i}
+                );
+                ++$i;
+            }
+        }
+        return $contacts;
+    }
+
+    public function create($params, $post_data)
     {
         $request_url = "{$this->url}&api_action=campaign_create&api_output={$this->output}";
         $response = $this->curl($request_url, $post_data);
         return $response;
     }
 
-    function delete_list($params)
+    public function delete_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_delete_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function delete($params)
+    public function delete($params)
     {
         $request_url = "{$this->url}&api_action=campaign_delete&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function list_($params)
+    public function list_($params)
     {
         $request_url = "{$this->url}&api_action=campaign_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function paginator($params)
+    public function paginator($params)
     {
         $request_url = "{$this->url}&api_action=campaign_paginator&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_bounce_list($params)
+    public function report_bounce_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_bounce_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_bounce_totals($params)
+    public function report_bounce_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_bounce_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_forward_list($params)
+    public function report_forward_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_forward_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_forward_totals($params)
+    public function report_forward_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_forward_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_link_list($params)
+    public function report_link_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_link_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_link_totals($params)
+    public function report_link_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_link_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_open_list($params)
+    public function report_open_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_open_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_open_totals($params)
+    public function report_open_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_open_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_totals($params)
+    public function report_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_unopen_list($params)
+    public function report_unopen_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_unopen_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_unsubscription_list($params)
+    public function report_unsubscription_list($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_unsubscription_list&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function report_unsubscription_totals($params)
+    public function report_unsubscription_totals($params)
     {
         $request_url = "{$this->url}&api_action=campaign_report_unsubscription_totals&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function send($params)
+    public function send($params)
     {
         $request_url = "{$this->url}&api_action=campaign_send&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
         return $response;
     }
 
-    function status($params)
+    public function status($params)
     {
         $request_url = "{$this->url}&api_action=campaign_status&api_output={$this->output}&{$params}";
         $response = $this->curl($request_url);
